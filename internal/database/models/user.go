e39f2d67eed6b5d4ad0c8e56cdb60a1e52942ac4
mc-solo <revatoel@gmail.com>
2026-03-13 04:37:06 +0300

    create(models): add init models

---
+++ internal/database/models/user.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID           uuid.UUID  `gorm:"type:uuid;primary_key;default:gen_random_uuid()" json:"id"`
+	Email        string     `gorm:"size:255;uniqueIndex;not null" json:"email"`
+	Username     string     `gorm:"size:25;column:username;uniqueIndex" json:"username"`
+	PasswordHash string     `gorm:"column:password;not null" json:"-"`
+	FirstName    string     `gorm:"column:first_name;size:100" json:"first_name"`
+	LastName     string     `gorm:"column:last_name;size:100" json:"last_name"`
+	CreatedAt    *time.Time `gorm:"column:created_at;" json:"created_at"`
+	UpdatedAt    *time.Time `gorm:"column:updated_at;" json:"-"`
+	DeletedAt    *time.Time `gorm:"column:deleted_at;index" json:"-"`
+
+	// relations
+	Preferences *UserPreference `gorm:"foreignKey:UserID" json:"preferences,omitempty"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
