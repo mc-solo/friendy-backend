f3cc8fbc363fd7c55e3b44392a3ea1e24cf1b86e
mc-solo <revatoel@gmail.com>
2026-03-15 05:19:38 +0300

    fix: field name type and moves it into the models package

--- internal/database/models/user_preferences.go
+++ internal/database/models/user_preferences.go
@@ -1,10 +1,10 @@
-package database
+package models
 
 import (
 	"time"
 )
 
 type UserPreference struct {
-	CreatedAT *time.Time `gorm:"column:created_at" json:"created_at"`
+	CreatedAt *time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt *time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
