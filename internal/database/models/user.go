5cda105dd15372b6705a8546c0183f7cd08226af
mc-solo <revatoel@gmail.com>
2026-03-19 01:50:37 +0300

    update: add default values to time stamps

--- internal/database/models/user.go
+++ internal/database/models/user.go
@@ -7,15 +7,16 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID  `gorm:"type:uuid;primarykey;default:gen_random_uuid()" json:"id"`
-	Email        string     `gorm:"size:255;uniqueIndex;not null" json:"email"`
-	Username     string     `gorm:"size:25;column:username;uniqueIndex" json:"username"`
-	PasswordHash string     `gorm:"column:password_hash;not null" json:"-"`
-	FirstName    string     `gorm:"column:first_name;size:100" json:"first_name"`
-	LastName     string     `gorm:"column:last_name;size:100" json:"last_name"`
-	CreatedAt    *time.Time `gorm:"column:created_at;default:now()" json:"created_at"`
-	UpdatedAt    *time.Time `gorm:"column:updated_at;default:now()" json:"-"`
-	DeletedAt    *time.Time `gorm:"column:deleted_at;index" json:"-"`
+	ID           uuid.UUID `gorm:"type:uuid;primarykey;default:gen_random_uuid()" json:"id"`
+	Email        string    `gorm:"size:255;uniqueIndex;not null" json:"email"`
+	Username     string    `gorm:"size:25;column:username;uniqueIndex" json:"username"`
+	PasswordHash string    `gorm:"column:password_hash;not null" json:"-"`
+	FirstName    string    `gorm:"column:first_name;size:100" json:"first_name"`
+	LastName     string    `gorm:"column:last_name;size:100" json:"last_name"`
+
+	CreatedAt time.Time  `gorm:"column:created_at;default:now()" json:"created_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at;default:now()" json:"-"`
+	DeletedAt *time.Time `gorm:"column:deleted_at;index" json:"-"`
 
 	// relations
 	Preferences *UserPreference `gorm:"foreignKey:UserID" json:"preferences,omitempty"`
